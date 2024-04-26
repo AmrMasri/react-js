1f5b9946a3574ea2d09e2f850fe80d275a5ecd5a
amr <amrmasri999@gmail.com>
2024-04-26 18:13:52 +0300

    creating array and order it by map

--- src/list.jsx
+++ src/list.jsx
@@ -1,9 +1,18 @@
 
 function List(){
 
-    const fruits = ["apple", "banana", "coconut", "pineapple", "orange"];
+    const fruits = [{id: 1, name: "apple", calories: 95},
+                    {id: 2, name: "banana", calories: 105},
+                    {id: 3, name: "coconut", calories: 159},
+                    {id: 4, name: "pineapple", calories: 37},
+                    {id: 5, name: "orange", calories: 45}];
 
-    const ListItems = fruits.map(fruit => <li>{fruit}</li>)
+   // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+    //  const highCalFruits = fruits.filter(fruit => fruit.calories >= 95);
+
+    const ListItems = fruits.map(fruit =>  <li key={fruit.id}>
+                                                {fruit.name} &nbsp; 
+                                             <b>{fruit.calories}</b></li>);
 
     return(<ol>{ListItems}</ol>);
 }
