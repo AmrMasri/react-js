b07ed2dddbd84b535aa55d102de9761ddc18e615
amr <amrmasri999@gmail.com>
2024-04-26 20:21:23 +0300

    dgdfgfhf

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <>
-      {fruits.length > 0 && <List items={fruits} category="Fruits" />}
+      {fruits.length > 0 && <List items={fruits} category="aww" />}
       {vegetables.length > 0 && (
         <List items={vegetables} category="Vegetables" />
       )}
