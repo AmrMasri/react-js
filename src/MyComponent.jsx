a304e53344ba8d302f371dc4cd83ac8145400d14
amr <amrmasri999@gmail.com>
2024-05-06 19:05:17 +0300

    ndsjj

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
 
 function MyComponent(){
 
+        const [car, setCar] = useState({year: 2024,make: "ford",model: "Mustang"})
+
+        function handleYearChange (event){
+            setCar( c => ({...c , year: event.target.value}))
+        }
+        function handleMakeChange (event){
+
+        }
+        function handleModelChange (event){
+
+        }
+
+        return(
+            <div>
+                <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
+
+                <input type="number" value={car.year} onChange={handleYearChange}/><br />
+                <input type="text" value={car.make} onChange={handleMakeChange} /><br />
+                <input type="text" value={car.model} onChange={handleModelChange} /><br />
+            </div>
+        );
 
 }
 export default MyComponent ;
