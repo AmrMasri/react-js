f5eaf0032556c1ec17665a8df575477ddafcfdf7
amr <amrmasri999@gmail.com>
2024-05-09 16:06:47 +0300

    creating new project

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,7 +3,12 @@ import { useState } from "react";
 function MyComponent(){
 
 
-    
+    const [cars, setCars] = useState ([]);
+    const [carYear, setCarYear] = useState(new Date().getFullYear());
+    const [carMake, setCarMake] = useState("")
+    const [carModel, setCarModel] = useState("")
+
+
 
     return(<></>);
 
