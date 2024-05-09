98368890014e2dbe4bc591240c2030f918ebec7f
amr <amrmasri999@gmail.com>
2024-05-09 15:51:57 +0300

    new learning

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,28 +2,10 @@ import { useState } from "react";
 
 function MyComponent(){
 
-    const [foods, setFoods] = useState(["banana", "apple", "orange"]);
 
-    function handleAddFood (){
-        const newFood = document.getElementById("foodsInput").value
-        document.getElementById("foodsInput").value = ""
-        setFoods (f => [...f ,newFood])
-    }
+    
 
-    function handleRemoveFood (index){
-        setFoods(foods.filter(( _ , i) => i !== index))
-    }
-
-    return(<div>
-             <h2>List Of Food</h2>
-             <ul>
-                {foods.map((food , index) => <li key={index} onClick={() => handleRemoveFood (index)}>{food}</li>)}
-             </ul>
-
-             <input type="text" id="foodsInput" placeholder="Enter Food Name"/>
-
-             <button on onClick={handleAddFood}>Add Food</button>
-        </div>);
+    return(<></>);
 
 }
 export default MyComponent ;
