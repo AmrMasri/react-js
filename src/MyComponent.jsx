81695f6ca3d7cdf1f98c680a19cf9dff80d11522
amr <amrmasri999@gmail.com>
2024-05-17 17:39:07 +0300

    adding functions

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,8 +2,35 @@ import { useState, useEffect} from "react";
 
 function MyComponent(){
 
+    const [count, setCount] = useState(0);
+    const [color, setColor] = useState("green")
+
+    useEffect(() => {
+        document.title = `count : ${count} ${color}`
+    }, [count]);
+
+
+
+    function addCount(){
+        setCount (c => c + 1)
+    }
+
+    function subtractCount(){
+        setCount (c => c - 1)
+    }
+
+    function changeColor(){
+        setColor(c => c === "green" ? "red" : "green")
+    }
+
     return(
-        <></>
+        <>
+        <p style={{color: color}}>count: {count}</p>
+        <button onClick={addCount}>Add</button>
+        <button onClick={subtractCount}>Subtract</button><br />
+        <button onClick={changeColor}>change color</button>
+
+        </>
     );
 }
 export default MyComponent ;
