6c4800d0cd9855aff5a45998c93f2a273d8d1b46
amr <amrmasri999@gmail.com>
2024-05-17 19:20:36 +0300

    adding event listner

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -5,10 +5,26 @@ function MyComponent(){
     const [width, setWidth] = useState(window.innerWidth)
     const [Height, setHeight] = useState(window.innerHeight)
 
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        console.log("Event Listner Added")
+
+        return() => {
+            window.removeEventListener("resize", handleResize);
+            console.log("Event Listner Removed")
+        }
+    }, [])
+
+    function handleResize(){
+        setWidth(window.innerWidth)
+        setHeight(window.innerHeight)
+    }
+
 
     return(
         <>
-        
+        <p>Window Width : {width}px</p>
+        <p>Window Height : {Height}px</p>
         </>
     );
 }
