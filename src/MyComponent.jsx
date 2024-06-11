48787a3fbe32c4e84be6188f3ad517d7e66cdf61
amr <amrmasri999@gmail.com>
2024-06-11 17:55:29 +0300

    fgds

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,23 +2,49 @@ import { useState, useEffect, useRef } from "react";
 
 function MyComponent (){
 
-    const inputRef = useRef(null);
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
+
 
     useEffect(() => {
         console.log("Component Rendered");
-        console.log(inputRef)
     })
 
-    function handleClick(){
-        inputRef.current.focus();
+    function handleClick1(){
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor = "yellow";
     }
+
+    
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef2.current.style.backgroundColor = "yellow";
+    }
+
+    
+    function handleClick3(){
+        inputRef3.current.focus();
+        inputRef3.current.style.backgroundColor = "yellow";
+    }
+
 
     return(
         <div>
-        <button onClick={handleClick}>
-            Click me!
+        <button onClick={handleClick1}>
+            Click me 1 !
         </button>
-        <input ref={inputRef} />
+        <input ref={inputRef1} /> 
+
+        <button onClick={handleClick2}>
+            Click me 2 !
+        </button>
+        <input ref={inputRef2} /> 
+
+        <button onClick={handleClick3}>
+            Click me 3 !
+        </button>
+        <input ref={inputRef3} /> 
         </div>
     );
 }
