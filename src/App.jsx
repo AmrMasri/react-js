6c3045b40ba18d7178c7abd9f6a4d7094c176fd1
amr <amrmasri999@gmail.com>
2024-06-19 11:06:37 +0300

    sdfkjkp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import HomePage from "./HomePage";
 import MyComponent from "./MyComponent";
-import {createBrowserRouter} from 'react-router-dom'
 
 function App() {
-  const router =createBrowserRouter([])
-  return (<HomePage/>);
+
+  return (<></>);
   
 }
 
