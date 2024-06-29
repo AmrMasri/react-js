ff3f31668983ae0609616ca0fd17352313398de5
amr <amrmasri999@gmail.com>
2024-06-29 14:11:23 +0300

    create files

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,7 @@ import './index.css'
 import {BrowserRouter} from 'react-routet-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
 )
