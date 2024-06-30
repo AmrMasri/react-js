ceb0faff5de1ff63f0bc29304a8015676d2ad8ba
amr <amrmasri999@gmail.com>
2024-06-30 12:12:08 +0300

    adkkoksowk

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
-
-  return <h1>Hello World</h1>
-
-  
-  
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route />
+    </Routes>
+  );
 }
 
 export default App;
