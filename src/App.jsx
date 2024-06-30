06a6fff1bce538b1dbb8d5bfe41fda2b8bb5ab40
amr <amrmasri999@gmail.com>
2024-06-30 12:59:03 +0300

    dsfijjkl;

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { Link ,Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { BookList } from "./pages/BookLIst";
+import { Book } from "./pages/Book";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route />
-    </Routes>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Books" element={<BookList />} />
+        <Route path="/Books/:id" element={<Book />} />
+      </Routes>
+    </>
   );
 }
 
